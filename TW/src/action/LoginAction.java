0cf05d8ba955ab332542b600774d53c0d9d7c773
bhwjc002 <bhwjc002@gmail.com>
2010-04-16 11:04:33 +0000

    1. 修改linker.jsp   2. 新建dao.hibernateTemplate包，新增了DAO层的基本CRUD的模板类（若想要新增查询方法，可在dao包中的子类增加）

--- TW/src/action/LoginAction.java
+++ TW/src/action/LoginAction.java
@@ -1,10 +1,15 @@
 package action;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import bean.Article;
+
 import com.opensymphony.xwork2.ActionSupport;
+
+import dao.ArticleDao;
 
 
 public class LoginAction extends ActionSupport{
@@ -17,6 +22,16 @@ public class LoginAction extends ActionSupport{
 	public String execute()throws Exception {
 		if(username.equals("1")&&password.equals("1"))
 			return SUCCESS;	
+		
+		/**
+		 * test the hibernate CRUD
+		 */
+//		ArticleDao articleDao=new ArticleDao();
+//		List<Article> articles=articleDao.getAllEntity();
+//		for(Article article:articles){
+//			System.out.println(article.getId()+" : "+article.getTitle()+" : "+article.getContent());
+//		}
+	
 		
 		return INPUT;
 		
