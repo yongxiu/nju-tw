8e227052fc283309830bc91c4a925217c361d5c7
bhwjc002 <bhwjc002@gmail.com>
2010-04-20 06:22:48 +0000

    update news

--- TW/src/action/UpdateNewsAction.java
+++ TW/src/action/UpdateNewsAction.java
@@ -1,5 +1,38 @@
 package action;
 
-public class UpdateNewsAction {
+import java.util.ArrayList;
 
+import service.GetFiveNews;
+
+import bean.GenericArticle;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UpdateNewsAction extends ActionSupport{
+	/**
+	 * wjc 
+	 * usage: update news
+	 */
+	private static final long serialVersionUID = 5843553509139320770L;
+	//five news to be displayed
+	ArrayList<GenericArticle> news;
+	
+	//Get service
+	
+	public String execute(){
+		GetFiveNews service = new GetFiveNews();
+		news=service.getFiveNews();
+		return SUCCESS;
+	}
+
+	public ArrayList<GenericArticle> getNews() {
+		return news;
+	}
+
+	public void setNews(ArrayList<GenericArticle> news) {
+		this.news = news;
+	}
+	
+	
+	
 }
