5d62bfa64521864f74d4033fd683ddb89fe1f049
yongxiu <cuiyongxiu@gmail.com>
2010-04-23 15:24:10 +0000

    再增加一个上传文件测试

--- TW/src/action/FileUploadAction.java
+++ TW/src/action/FileUploadAction.java
@@ -13,80 +13,80 @@ import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class FileUploadAction extends ActionSupport  {
-    private static final long serialVersionUID = 572146812454l ;
-    private static final int BUFFER_SIZE = 16 * 1024 ;
-   
-    private File myFile;
-   
+public class FileUploadAction extends ActionSupport {
+	private static final long serialVersionUID = 572146812454l;
+	private static final int BUFFER_SIZE = 16 * 1024;
 
-	private String contentType;
+	private File myFile;
+    private String contentType;
     private String fileName;
     private String imageFileName;
     private String caption;
    
-    public void setMyFileContentType(String contentType)  {
-        this .contentType = contentType;
-   } 
+    public void setMyFileContentType(String contentType) {
+        this.contentType = contentType;
+   }
    
-    public void setMyFileFileName(String fileName)  {
-        this .fileName = fileName;
-   } 
+    public void setMyFileFileName(String fileName) {
+        this.fileName = fileName;
+   }
        
-    public void setMyFile(File myFile)  {
-        this .myFile = myFile;
-   } 
-    public File getMyFile() {
-		return myFile;
-	}
+    public void setMyFile(File myFile) {
+        this.myFile = myFile;
+   }
    
-    public String getImageFileName()  {
+    public String getImageFileName() {
         return imageFileName;
-   } 
+   }
    
-    public String getCaption()  {
+    public String getCaption() {
         return caption;
+   }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
    } 
 
-     public void setCaption(String caption)  {
-        this .caption = caption;
-   } 
-   
-    private static void copy(File src, File dst)  {
-        try  {
-           InputStream in = null ;
-           OutputStream out = null ;
-            try  {                
-               in = new BufferedInputStream( new FileInputStream(src), BUFFER_SIZE);
-               out = new BufferedOutputStream( new FileOutputStream(dst), BUFFER_SIZE);
-                byte [] buffer = new byte [BUFFER_SIZE];
-                while (in.read(buffer) > 0 )  {
-                   out.write(buffer);
-               } 
-            } finally  {
-                if ( null != in)  {
-                   in.close();
-               } 
-                 if ( null != out)  {
-                   out.close();
-               } 
-           } 
-        } catch (Exception e)  {
-           e.printStackTrace();
-       } 
-   } 
-   
-    private static String getExtention(String fileName)  {
-        int pos = fileName.lastIndexOf(".");
-        return fileName.substring(pos);
-   } 
+	private static void copy(File src, File dst) {
+		try {
+			InputStream in = null;
+			OutputStream out = null;
+			try {
+				out = new BufferedOutputStream(new FileOutputStream(dst),
+						BUFFER_SIZE);
+				in = new BufferedInputStream(new FileInputStream(src),
+						BUFFER_SIZE);
 
-   @Override
-   public String execute() {
+				byte[] buffer = new byte[BUFFER_SIZE];
+				while (in.read(buffer) > 0) {
+					out.write(buffer);
+				}
+			} finally {
+				if (null != in) {
+					in.close();
+					
+				}
+				if (null != out) {
+					out.close();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String getExtention(String fileName) {
+		System.out.println(fileName);
+		int pos = fileName.lastIndexOf(".");
+		return fileName.substring(pos);
+	}
+	
+	@Override
+    public String execute()     {        
        imageFileName = new Date().getTime() + getExtention(fileName);
-       File imageFile = new File(ServletActionContext.getServletContext().getRealPath("/UserFiles") + "/" + imageFileName);
+       File imageFile = new File(ServletActionContext.getServletContext().getRealPath( "/UserFiles") + "/" + imageFileName);
        copy(myFile, imageFile);
-       return SUCCESS;
-   } 
-   
-} 
+        return SUCCESS;
+   }
+
+}
