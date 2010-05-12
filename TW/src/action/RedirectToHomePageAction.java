231506ab4ff87a5158e06893ebdbdf0c4bc77d64
bhwjc002 <bhwjc002@gmail.com>
2010-05-12 07:49:57 +0000

    修正小bug

--- TW/src/action/RedirectToHomePageAction.java
+++ TW/src/action/RedirectToHomePageAction.java
@@ -26,30 +26,30 @@ public class RedirectToHomePageAction extends ActionSupport{
 		 * wjc
 		 * test lucene
 		 */
-		String INDEX_PATH = ServletActionContext.getServletContext().getRealPath("/")+"index";
-		IndexWriter writer;
-		try {
-			writer = new IndexWriter(INDEX_PATH,new StandardAnalyzer(), true);
-			Document doc = new Document();
-			Field f1 = new Field("a","a1",Field.Store.YES,Field.Index.TOKENIZED);
-			doc.add(f1);
-			writer.addDocument(doc);
-			writer.close();
-			
-			IndexSearcher searcher = new IndexSearcher(INDEX_PATH);
-			Term term = new Term("a","a1");
-			TermQuery query = new TermQuery(term);
-			
-			Hits hits = searcher.search(query);
-			for(int i=0;i<hits.length();i++) {
-				System.out.println(hits.doc(i));
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		System.out.println(INDEX_PATH);
+//		String INDEX_PATH = ServletActionContext.getServletContext().getRealPath("/")+"index";
+//		IndexWriter writer;
+//		try {
+//			writer = new IndexWriter(INDEX_PATH,new StandardAnalyzer(), true);
+//			Document doc = new Document();
+//			Field f1 = new Field("a","a1",Field.Store.YES,Field.Index.TOKENIZED);
+//			doc.add(f1);
+//			writer.addDocument(doc);
+//			writer.close();
+//			
+//			IndexSearcher searcher = new IndexSearcher(INDEX_PATH);
+//			Term term = new Term("a","a1");
+//			TermQuery query = new TermQuery(term);
+//			
+//			Hits hits = searcher.search(query);
+//			for(int i=0;i<hits.length();i++) {
+//				System.out.println(hits.doc(i));
+//			}
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		System.out.println(INDEX_PATH);
 		
 		return SUCCESS;
 	}
