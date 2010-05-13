a5cece99e57dfcf4d9a7b119b6407685e9e21c2d
yongxiu <cuiyongxiu@gmail.com>
2010-05-13 15:09:58 +0000

    增加Email的js，修改Email的css

--- TW/WebContent/js/sendmail.js
+++ TW/WebContent/js/sendmail.js
@@ -1,101 +1,48 @@
-// JavaScript Document
-
-window.onload = initForms;
-
-function initForms(){
-	
-	for(var i = 0; i < document.forms.length; i++){
-		document.forms[i].onsubmit = function(){return validForm();};	
-	}
-}
-
-function validForm(){
-	var allGood = true;
-	var allTags = document.getElementsByTagName("*");
-	
-	for(var i = 0; i < allTags.length; i++){
-		if(!validTag(allTags[i])){
-			allGood = false;	
+$(document).ready(function() {
+	$('.inputname').blur(function() {
+		var $parent = $(this).parent();
+		$parent.find(".formtips").remove();
+		if(this.value=="") {
+			var errorMsg = '请输入您的称谓。';
+			$parent.append('<span class="formtips onError">' + errorMsg + '</span>');
 		}
-	}
-	return allGood;
-	
-	function validTag(thisTag){
-		var outClass = "";
-		var allClasses = thisTag.className.split(" ");
-	
-		for(var i= 0; i< allClasses.length; i ++){
-			outClass += validBasedOnClass(allClasses[i]) + " ";	
-		} 
-	
-		thisTag.className = outClass;
-		if(outClass.indexOf("invalid") > -1){
-			thisTag.focus();
-			if(thisTag.nodeName == "input" || thisTag.nodeName == "textarea"){
-				thisTag.select();	
-			}
-			
-			alert("输入有误，请正确填写后重新发送！");
+	});
+
+	$('.inputemail').blur(function() {
+		var $parent = $(this).parent();
+		$parent.find(".formtips").remove();
+		if(this.value=="") {
+			var errorMsg = '请输入您的Email。';
+			$parent.append('<span class="formtips onError">' + errorMsg + '</span>');
+		} else if(this.value !="" && !/.+@.+.+\.[a-zA-Z]{2,4}$/.test(this.value)) {
+			var errorMsg = '请输入正确的Email地址。';
+			$parent.append('<span class="formtips onError">' + errorMsg + '</span>');
+		}
+	});
+
+	$('.inputurl').blur(function() {
+		var $parent = $(this).parent();
+		$parent.find(".formtips").remove();
+		if(this.value=="") {
+			var errorMsg = '请输入主题。';
+			$parent.append('<span class="formtips onError">' + errorMsg + '</span>');
+		}
+	});
+
+	$('.textarea').blur(function() {
+		var $parent = $(this).parent();
+		$parent.find(".formtips").remove();
+		if(this.value=="") {
+			var errorMsg = '请输入回复内容。';
+			$parent.append('<p><span class="formtips onError">' + errorMsg + '</span></p>');
+		}
+	});
+
+	$('#input_comments_submit').click(function() {
+		$("#form_comments .input").trigger('blur');
+		var numError = $("#form_comments .onError").length;
+		if(numError) {
 			return false;
-			
 		}
-			
-		return true;
-		function validBasedOnClass(thisClass){
-			var classBack = "";
-			switch(thisClass){
-			
-				case "":
-				case "invalid":	
-					break;
-				case "reqd":
-					if(allGood && thisTag.value == ""){
-						classBack += "invalid ";
-					}
-					classBack += thisClass;
-					break;
-				case "email":
-					if(allGood && !validEmail(thisTag.value)){
-						classBack += "invalid ";
-					}
-				default:
-					classBack += thisClass;
-			}
-			
-			return classBack;
-		}
-		
-		function validEmail(email){
-			var invalidChars = "/:,;";
-			if(email == ""){
-				return false;	
-			}
-			
-			for(var k = 0; k < invalidChars.length; k++){
-				var badChar = invalidChars.charAt(k);
-				if(email.indexOf(badChar) > -1){
-					return false;
-				}
-			}
-			
-			var atPos = email.indexOf("@", 1);
-			if(atPos == -1){
-				return false;	
-			}
-			
-			if(email.indexOf("@", atPos + 1) != -1){
-				return false;	
-			}
-			
-			var periodPos = email.indexOf(".", atPos);
-			if(periodPos == -1){
-				return false;	
-			}
-			
-			if(periodPos + 3 > email.length){
-				return false;	
-			}
-			return true;
-		}
-	}
-}
+	});
+});
