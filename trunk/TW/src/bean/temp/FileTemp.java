8528768f63b685c1c9fe15d58e17be017fb13740
cuiyongxiu <cuiyongxiu@f56478cc-1f8e-9c26-3059-65b3bdc8ccc5>
2010-05-22 16:26:54 +0000

    增加后台上传文件的列表和修改

---
+++ trunk/TW/src/bean/temp/FileTemp.java
@@ -0,0 +1,40 @@
+package bean.temp;
+
+import java.util.Date;
+
+
+
+public class FileTemp {
+	
+	private long id;
+	
+	private String filename;
+	
+	private Date date;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+}
