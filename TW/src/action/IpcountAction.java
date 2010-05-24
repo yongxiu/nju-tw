4b544ee83712dae4830d6016af2828d38b0d94be
GeraldXv <gerald.xv@gmail.com>
2010-05-24 05:53:18 +0000

    修改了ipcount

--- TW/src/action/IpcountAction.java
+++ TW/src/action/IpcountAction.java
@@ -6,39 +6,58 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
-import bean.Article;
+
+
+import bean.IpCount;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-import dao.ArticleDao;
+import dao.IpCountDao;
+
 
 
 public class IpcountAction extends ActionSupport{
-	
-     long count=1054;
-      int v=0;
-      String countString="<br />";
-	
-	public String getCountString() {
-		return countString;
+	private long count;
+
+      public long getCount() {
+		return count;
 	}
 
-	public void setCountString(String countString) {
-		this.countString = countString;
+	public void setCount(long count) {
+		this.count = count;
 	}
+
+	int v=0;
+      String countString="<br />";
+	
+
 
 	private static final long serialVersionUID = 1L;
 
 	public String execute()throws Exception {
-		
+		IpCountDao dao = new IpCountDao();
+		IpCount ipCount = dao.getById(1);
+		count = ipCount.getCount();
+		ipCount.setCount(count+1);
+		dao.update(ipCount);
 		while(count>0){
 		
 			countString="<img src='/TW/ipImage/"+count%10+".gif' />"+countString;
 			count=count/10;
 			System.out.println(new Date());
 		}
+		
+		
 		return SUCCESS;
 		
+	}
+
+	public String getCountString() {
+		return countString;
+	}
+
+	public void setCountString(String countString) {
+		this.countString = countString;
 	}
 	
 }
