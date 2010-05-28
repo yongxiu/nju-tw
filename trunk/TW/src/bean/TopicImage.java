4dd48b4c35ebaae45a1bc637332addd8d4a9a1e7
cuiyongxiu <cuiyongxiu@f56478cc-1f8e-9c26-3059-65b3bdc8ccc5>
2010-05-28 15:37:03 +0000

    活动图片上传实现和数据库更新

---
+++ trunk/TW/src/bean/TopicImage.java
@@ -0,0 +1,27 @@
+package bean;
+
+public class TopicImage {
+	private int id;
+	private String path;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public TopicImage(String path) {
+		
+		this.path = path;
+	}
+	public TopicImage() {
+		
+	}
+	
+	
+}
---
+++ trunk/TW/src/dao/TopicImageDao.java
@@ -0,0 +1,8 @@
+package dao;
+
+import bean.File;
+import dao.hibernateTemlate.HibernateGenericDao;
+import bean.TopicImage;
+public class TopicImageDao extends HibernateGenericDao<TopicImage,Integer>{
+
+}
