dd68944f9be394864736358ccb0658a691f43f39
javoft <huatingzl@gmail.com>
2010-05-30 02:50:53 +0000

    变灰 和恢复

--- TW/src/action/admin/TurnOriAction.java
+++ TW/src/action/admin/TurnOriAction.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
@@ -30,25 +32,72 @@ public class TurnOriAction extends ActionSupport {
 		String currentDirPath = ServletActionContext.getServletContext()
 				.getRealPath("/");
 
-		File source = new File(currentDirPath + "css/originalstyle.css");
-		File desc = new File(currentDirPath + "css/style.css");
+		File source = new File(currentDirPath + "css/style.css");
+		File tmp = new File(currentDirPath + "css/tmp.css");
 
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-				new FileInputStream(source)));
-
-		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
-				new FileOutputStream(desc)));
-
-		String str = null;
-		while ((str = in.readLine()) != null) {
-			out.write(str + "\r\n");
-		}
-		out.close();
-		in.close();
-
+		changeBack(source, tmp);
 		message = "主题已经恢复原主题，请刷新";
 
 		return SUCCESS;
+	}
+
+	public void changeBack(File source, File tmpFile) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					new FileInputStream(source)));
+			String tmp = "";
+			String grayCSS = "";
+			String normalCSS = "";
+			while ((tmp = br.readLine()) != null) {
+				if (tmp.length() == 0) {
+					break;
+				}
+				grayCSS += (tmp + "\n");
+			}
+			while ((tmp = br.readLine()) != null) {
+				normalCSS += (tmp + "\n");
+			}
+			br.close();
+
+			System.out.println(grayCSS);
+			if (grayCSS.charAt(0) != '/') {
+				grayCSS = "/*" + grayCSS + "*/" + "\n";
+
+				BufferedWriter bo = new BufferedWriter(new OutputStreamWriter(
+						new FileOutputStream(tmpFile)));
+				bo.append(grayCSS + "\n" + normalCSS);
+				bo.flush();
+				bo.close();
+				updateCSS(source, tmpFile);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void updateCSS(File source, File tmpFile) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					new FileInputStream(tmpFile)));
+			String tmp = "";
+			String CSS = "";
+			while ((tmp = br.readLine()) != null) {
+				CSS += (tmp + "\n");
+			}
+			br.close();
+			BufferedWriter bo = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(source)));
+
+			bo.append(CSS);
+			bo.flush();
+			bo.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
