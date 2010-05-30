b25a8d587a9fe167f51329a98a0f209ebd516d1d
huatingzl <huatingzl@f56478cc-1f8e-9c26-3059-65b3bdc8ccc5>
2010-05-30 03:58:15 +0000

    正常的CSS放在  tmp里    滤镜放在  string里

--- trunk/TW/src/action/admin/TurnGrayAction.java
+++ trunk/TW/src/action/admin/TurnGrayAction.java
@@ -24,70 +24,37 @@ public class TurnGrayAction extends ActionSupport {
 		String currentDirPath = ServletActionContext.getServletContext()
 				.getRealPath("/");
 
-		String source = currentDirPath + "css/style.css";
+		String style = currentDirPath + "css/style.css";
 		String tmp = currentDirPath + "css/tmp.css";
 
-		changeGray(source, tmp);
+		changeGray(style, tmp);
 
 		message = "主题已经变为灰色，请刷新";
 
 		return SUCCESS;
 	}
 
-	public void changeGray(String source, String tmpFile) {
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					new FileInputStream(source)));
-			String tmp = "";
-			String grayCSS = "";
-			String normalCSS = "";
-			while ((tmp = br.readLine()) != null) {
-				if (tmp.length() == 0) {
-					break;
-				}
-				grayCSS += (tmp + "\n");
-			}
-			while ((tmp = br.readLine()) != null) {
-				normalCSS += (tmp + "\n");
-			}
-			br.close();
-			if (grayCSS.charAt(0) == '/') {
-				grayCSS = grayCSS.substring(2, grayCSS.length() - 3);
-				BufferedWriter bo = new BufferedWriter(new OutputStreamWriter(
-						new FileOutputStream(tmpFile)));
-				bo.append(grayCSS + "\n" + normalCSS);
-				bo.flush();
-				bo.close();
+	public void changeGray(String style, String tmpFile) {
 
-			}
-			updateCSS(source, tmpFile);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void updateCSS(String source, String tmpFile) {
+		String filter = "html { filter:progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);}";
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(
 					new FileInputStream(tmpFile)));
 			String tmp = "";
-			String CSS = "";
+			String normalCSS = "";
 			while ((tmp = br.readLine()) != null) {
-				CSS += (tmp + "\n");
+				normalCSS += (tmp + "\n");
 			}
 			br.close();
 			BufferedWriter bo = new BufferedWriter(new OutputStreamWriter(
-					new FileOutputStream(source)));
-			bo.append(CSS);
+					new FileOutputStream(style)));
+			bo.append(filter + "\n" + normalCSS);
+			
+			System.out.println(filter + "\n" + normalCSS);
 			bo.flush();
 			bo.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
