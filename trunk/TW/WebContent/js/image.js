7da094f5a14f1032128beba4a310804e3a1d8506
cuiyongxiu <cuiyongxiu@f56478cc-1f8e-9c26-3059-65b3bdc8ccc5>
2010-05-31 11:27:50 +0000

    剪切图片的实现，topic冲突解决

--- trunk/TW/WebContent/js/image.js
+++ trunk/TW/WebContent/js/image.js
@@ -15,7 +15,12 @@ $(document).ready(function() {
     $('#preview img').css({
         width: Math.round(scaleX * $('#myimg').width()),
         height: Math.round(scaleY * $('#myimg').height()),
-    }); 
+    });
+	
+	$('#x1').val(0);
+	$('#y1').val(0);
+	$('#x2').val(w);
+	$('#y2').val(h);
 });
 
 function preview(img, selection) {
@@ -30,7 +35,12 @@ function preview(img, selection) {
         height: Math.round(scaleY * $('#myimg').height()),
         marginLeft: -Math.round(scaleX * selection.x1),
         marginTop: -Math.round(scaleY * selection.y1)
-    });  
+    });
+	
+	$('#x1').val(selection.x1);
+	$('#y1').val(selection.y1);
+	$('#x2').val(selection.x2);
+	$('#y2').val(selection.y2);
 }
 
 $(function () {
---
+++ trunk/TW/src/service/ImageService.java
@@ -0,0 +1,50 @@
+package service;
+
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Iterator;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReadParam;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+import javax.imageio.stream.ImageOutputStream;
+
+public class ImageService {
+
+	/** 截取图片 */
+	public File cutting(File file, int x, int y, int width, int height) {
+		try {
+			String endName = file.getName();
+			endName = endName.substring(endName.lastIndexOf(".") + 1);
+			Iterator<ImageReader> readers = ImageIO
+					.getImageReadersByFormatName(endName);
+			ImageReader reader = (ImageReader) readers.next();
+			InputStream is = new FileInputStream(file);
+			ImageInputStream iis = ImageIO.createImageInputStream(is);
+			reader.setInput(iis, true);
+			ImageReadParam param = reader.getDefaultReadParam();
+			Rectangle rect = new Rectangle(x, y, width, height);
+			param.setSourceRegion(rect);
+			BufferedImage bi = reader.read(0, param);
+			String path = file.getAbsolutePath();
+			path = path.substring(0, path.lastIndexOf("\\"));
+			//path=path + "1";
+			File newFile = new File(path);
+			if (!newFile.exists())
+				newFile.mkdirs();
+			newFile = new File(path, file.getName());
+			ImageOutputStream out = ImageIO
+					.createImageOutputStream(new FileOutputStream(newFile));
+			ImageIO.write(bi, endName, out);
+			file = newFile;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return file;
+	}
+
+}
