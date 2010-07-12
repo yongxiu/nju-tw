95cefa33394823624494cc9595787fd06e8b15a7
bhwjc002 <bhwjc002@gmail.com>
2010-07-12 02:08:06 +0000

    重点工作和品牌项目删除功能正常

--- TW/src/action/brand/DeleteBrandAction.java
+++ TW/src/action/brand/DeleteBrandAction.java
@@ -1,14 +1,17 @@
 package action.brand;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.apache.struts2.ServletActionContext;
 
 import bean.Brand;
+import bean.GenericArticle;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 import dao.BrandDao;
+import dao.GenericArticleDao;
 
 public class DeleteBrandAction extends ActionSupport{
 	/**
@@ -20,9 +23,22 @@ public class DeleteBrandAction extends ActionSupport{
 	 * CYX
 	 */
 	private int id;
+	private String message;
 	public String execute() {
 		BrandDao brandDao = new BrandDao();
+		GenericArticleDao articleDao = new GenericArticleDao();
+		ArrayList<GenericArticle> articles = articleDao.getArticlesByBrandId(id);
 		Brand brand = brandDao.getById(getId());
+		if(brand.getTop()==1) {
+			setMessage("(删除失败)！首页的品牌项目主题不可删除！请先替换别的品牌项目主题");
+			return ERROR;
+		}
+		//set brandit to zero
+		for(GenericArticle article:articles) {
+			article.setBrandid(0);
+			articleDao.update(article);
+		}
+		
 		String currentPath = ServletActionContext.getServletContext().getRealPath(brand.getPath().substring(3));
 
 		boolean success = new File(currentPath).delete();
@@ -36,6 +52,12 @@ public class DeleteBrandAction extends ActionSupport{
 	}
 	public void setId(int id) {
 		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
 	}
 	
 	
--- TW/src/action/iwork/DeleteiworkAction.java
+++ TW/src/action/iwork/DeleteiworkAction.java
@@ -1,13 +1,16 @@
 package action.iwork;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.apache.struts2.ServletActionContext;
 
+import bean.GenericArticle;
 import bean.IWork;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import dao.GenericArticleDao;
 import dao.IWorkDao;
 import dao.UserDao;
 
@@ -17,9 +20,23 @@ public class DeleteiworkAction extends ActionSupport{
 	 * CYX
 	 */
 	private int id;
+	private String message;
 	public String execute() {
 		IWorkDao iworkDao = new IWorkDao();
 		IWork iwork = iworkDao.getById(getId());
+		GenericArticleDao articleDao = new GenericArticleDao();
+		ArrayList<GenericArticle> articles = articleDao.getArticlesByIWorkId(id);
+		if(iwork.getTop()==1) {
+			setMessage("(删除失败)！首页的重点工作主题不可删除！请先替换别的重点工作主题");
+			return ERROR;
+		}
+		//set iworkid to zero
+		for(GenericArticle article:articles) {
+			article.setIworkid(0);
+			articleDao.update(article);
+		}
+		
+		
 		String currentPath = ServletActionContext.getServletContext().getRealPath(iwork.getPath().substring(3));
 		System.out.println(currentPath);
 		boolean success = new File(currentPath).delete();
@@ -33,6 +50,12 @@ public class DeleteiworkAction extends ActionSupport{
 	}
 	public void setId(int id) {
 		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
 	}
 	
 	
